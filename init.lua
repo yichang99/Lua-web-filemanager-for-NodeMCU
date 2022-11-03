7bcf06b22d131036fbfcf0e202ec5a361a44d108
yichang <yichang@xuanjianai.com>
2022-11-03 12:43:02 +0800

    First commit

---
+++ init.lua
@@ -0,0 +1,26 @@
+-- Yichang 2022-11-02 https://github.com/reg64
+
+cfg={
+   ssid="yourssid",
+   pwd="yourpassword"
+}
+
+print('\n\nSetting up WIFI...')
+wifi.setmode(wifi.STATION)
+oldcfg=wifi.sta.getconfig(true)
+if oldcfg ~= nil and oldcfg.ssid ~= "" then
+   cfg = oldcfg
+end
+wifi.sta.config(cfg)
+wifi.sta.connect()
+
+t1 = tmr.create()
+t1:alarm(1000, tmr.ALARM_AUTO, function()
+   if wifi.sta.getip() == nil then
+      print("Connecting to "..cfg.ssid)
+   else
+      t1:stop()
+      print("Connected. IP: "..wifi.sta.getip())
+      dofile("http.lua")
+   end
+end)
